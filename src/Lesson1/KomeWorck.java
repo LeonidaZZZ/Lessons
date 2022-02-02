7003d1b0c871c0d0929a63013a9ed9c88665ea55
Amd <mario95rus@gmail.com>
2022-02-03 00:04:11 +0600

    Salam

--- src/Lesson1/KomeWorck.java
+++ src/Lesson1/KomeWorck.java
@@ -1,7 +1,43 @@
 package Lesson1;
 
-public class KomeWorck {
+class HomeWork {
+//    public static void main(String[] args) {
+//        System.out.println ("HomeWork exercise 1");
+//
+//        int a = 20;
+//
+//        if (a > 0 && a < 10) {
+//            System.out.println("число положительное однозначное");
+//        }
+//        if (a >= 10 || a < 100) {
+//            System.out.println("Число положительгое двузначное");
+//        }
+//        if (a < 0 && a > -10) {
+//            System.out.println("Число отрицательное однозначное");
+//        }
+//        if (a <= -10 || a > -100) {
+//            System.out.println("Число отрицательное двузначное");  // тут гдето ошибка, Result 13 и 19 строки
+//        }
+//
+//    }
+
+//    public static void main(String[] args) {
+//        System.out.println("HomeWork ex2");
+//
+//        int a = 17;
+//        int b = 10;
+//        int c = 7;
+//
+//        if (a + b >= c && a + c >= b && b + c >= a) {
+//            System.out.println("Треугольник существует");
+//        }
+//        else {
+//            System.out.println("Треугольник не существует");
+//        }
+//    }
+
     public static void main(String[] args) {
+        System.out.println("HomeWork ex3");
 
     }
 }
