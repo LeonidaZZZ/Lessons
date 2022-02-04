9d27a36ee3b9e9ec855e0c9577060210b2a33e44
mrcon <DavidRossadnev@mail.ru>
2022-02-04 21:59:13 +0600

    Домашнее задание №1

--- src/Lesson1/HomeWork.java
+++ src/Lesson1/HomeWork.java
@@ -2,125 +2,183 @@ package Lesson1;
 
 public class HomeWork {
     public static void main(String[] args) {
-        int ff = 762;
-        if (ff > 0) {
-            System.out.println("Положительное число");
-        } else if (ff < 0) {
-            System.out.println("Отрицательное число");
-        }
+//        int ff = 762;
+//        if (ff > 0) {
+//            System.out.println("Положительное число");
+//        } else if (ff < 0) {
+//            System.out.println("Отрицательное число");
+//        }
+//
+//        if (ff < 10) {
+//            System.out.println("Однозначное число");
+//
+//        } else if (ff >= 10) {
+//            System.out.println("Двухзначное число");
+//        }
+//
+//
+//        int a = 6;
+//        int b = 6;
+//        int c = 10;
+//
+//        if ((a + b > c) && (c + b > a) && (a + c > b)) {
+//            System.out.println("Треугольник существует");
+//        } else {
+//            System.out.println("Треугольник не существует");
+//        }
+//
+//        int n = -1;
+//        if (n < 0) {
+//            n = n - 2;
+//        } else if (n > 0) {
+//            n = n + 1;
+//        } else {
+//            n = 10;
+//        }
+//        System.out.println(n);
+//
+//        int y = 1;
+//        int x = 1;
+//        int z = -1;
+//        int countPos = 0;
+//        int countNeg = 0;
+//        if (y > 0) {
+//            countPos++;
+//        } else if (y != 0) {
+//            countNeg++;
+//        }
+//        if (x > 0) {
+//            countPos++;
+//        } else if (x != 0) {
+//            countNeg++;
+//        }
+//        if (z > 0) {
+//            countPos++;
+//        } else if (z != 0) {
+//            countNeg++;
+//        }
+//        System.out.println("Положительных " + countPos + " Отрицательных" + countNeg);
+//
+//        if (y > x) {
+//            System.out.println(y + " Большое чилос ");
+//        } else if (y == x) {
+//            System.out.println("Числа равны");
+//        } else {
+//            System.out.println(x + " Большое число ");
+//        }
+//        int countProgramer = 53;
+//        if (countProgramer % 100 / 10 == 1) {
+//            System.out.println(countProgramer + " Программистов ");
+//        } else {
+//            switch (countProgramer % 10) {
+//                case 1:
+//                    System.out.println(countProgramer + " Программист ");
+//                    break;
+//                case 2:
+//                case 3:
+//                case 4:
+//                    System.out.println(countProgramer + " Программиста ");
+//                    break;
+//                default:
+//                    System.out.println(countProgramer + " Программистов ");
+//            }
+//
+//
+//        }
+//        int nn = 5;
+//        if (nn % 2 == 0) {
+//            System.out.println("Число четное");
+//
+//        } else {
+//            System.out.println("Число нечетное");
+//
+//        }
+//        if (nn > 10 && nn < 50) {
+//            System.out.println("ок");
+//
+//        } else {
+//            System.out.println("не ок");
+//
+//        }
+//
+//        switch (nn) {
+//            case 1:
+//                System.out.println("Понеделдьник");
+//                break;
+//            case 2:
+//                System.out.println("Вторник");
+//                break;
+//            case 3:
+//                System.out.println("Среда");
+//                break;
+//            case 4:
+//                System.out.println("Четверг");
+//                break;
+//            default:
+//                System.out.println("хер его знает что за день");
+//        }
+//        int i = 10;
+//        while (i >= 0) {
+//            System.out.println(i);
+//            i--;
+//
+//        }
+//
+//        do {
+//            System.out.println(i);
+//            i--;
+//        } while (i > 0);
+//
+//
+//        for (int df = 0; df < 10; df--) {
+//            System.out.println(df);
+//        }
+//        int p = 0;
+//        boolean isStop = true;
+//        while (isStop) {
+//            System.out.println(x);
+//            if (x == 10) {
+//                isStop = false;
+//            }
+//            p++;
+//        }
+//
+//        for (int j = 0; j < 10; j++) {
+//            for (int k = 0; k < 10; k++) {
+//
+//            }
+//        }
+//        int i1 = 10;
+//        int i2 = 3;
+//        double result = (double) i1 / (double) i2;
+//        System.out.println(result);
 
-        if (ff < 10) {
-            System.out.println("Однозначное число");
-
-        } else if (ff >= 10) {
-            System.out.println("Двухзначное число");
+        int gg = 142678;
+        int countR = 0;
+        while (gg > 0) {
+            gg = gg / 10;
+            countR++;
         }
+        System.out.println(countR);
 
-
-        int a = 6;
-        int b = 6;
-        int c = 10;
-
-        if ((a + b > c) && (c + b > a) && (a + c > b)) {
-            System.out.println("Треугольник существует");
-        } else {
-            System.out.println("Треугольник не существует");
-        }
-
-        int n = -1;
-        if (n < 0) {
-            n = n - 2;
-        } else if (n > 0) {
-            n = n + 1;
-        } else {
-            n = 10;
-        }
-        System.out.println(n);
-
-        int y = 1;
-        int x = 1;
-        int z = -1;
-        int countPos = 0;
-        int countNeg = 0;
-        if (y > 0) {
-            countPos++;
-        } else if (y != 0) {
-            countNeg++;
-        }
-        if (x > 0) {
-            countPos++;
-        } else if (x != 0) {
-            countNeg++;
-        }
-        if (z > 0) {
-            countPos++;
-        } else if (z != 0) {
-            countNeg++;
-        }
-        System.out.println("Положительных " + countPos + " Отрицательных" + countNeg);
-
-        if (y > x) {
-            System.out.println(y + " Большое чилос ");
-        } else if (y == x) {
-            System.out.println("Числа равны");
-        } else {
-            System.out.println(x + " Большое число ");
-        }
-    int countProgramer = 123;
-        if (countProgramer % 100 / 10 == 1){
-            System.out.println(countProgramer + " Программистов ");
-        }else {
-            switch (countProgramer % 10) {
-                case 1:
-                    System.out.println(countProgramer + " Программист ");
-                    break;
-                case 2:
-                case 3:
-                case 4:
-                    System.out.println(countProgramer + " Программиста ");
-                    break;
-                default:
-                    System.out.println(countProgramer + " Программистов ");
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.print(j * i +"\t" );
             }
+            System.out.println("");
+
+
+        }
+        int bb = 1;
+        for (int i = 0; i <20 ; i++) {
+            bb = bb * 2;
+            System.out.print(bb + " ");
+
 
 
 
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        int i1 = 10;
-        int i2 = 3;
-        double  result = (double) i1 / (double) i2;
-        System.out.println(result);
 
     }
-
 }
