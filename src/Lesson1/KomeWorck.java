ce67c2e8aef58fc9bdd0d9ecf12be16ca3d537ae
Amd <mario95rus@gmail.com>
2022-02-04 17:46:46 +0600

    Homework 2nd day

--- src/Lesson1/KomeWorck.java
+++ src/Lesson1/KomeWorck.java
@@ -72,7 +72,7 @@ class HomeWork {
 
         System.out.println("ex5");
 
-        int x1 = 19;
+        int x1 = 19;                    //Написать программу которая определит и выведет на экран большее число
         int y1 = 45;
 
         if (x1 > y1) {
@@ -90,7 +90,7 @@ class HomeWork {
 
         System.out.println(res);
 
-        System.out.println("ex7");
+        System.out.println("ex7");          //
 
         int programmerCount = 121;
         if (programmerCount % 100 / 10 == 1) {
@@ -111,7 +111,58 @@ class HomeWork {
                     System.out.println(programmerCount + " Програмистов");
             }
         }
+        System.out.println("HomeWwork 2 ex 1");     //Создать прог. проверяющую и ссобщ на экран чет или не чет
 
+        int n = 13;
+
+        if (n % 10 / 2 == 0) {
+            System.out.println("Число четное");
+        }
+        else {
+            System.out.println("Число нечетное");
+
+        }
+
+        System.out.println("ex 2");     // написать прог опред интервал 10:50 // как 1е и 2е зд объединить и надо ли это
+
+        int n1 = 13;
+
+        if (n1 > 10 && n1 < 50){
+            System.out.println("Число попадает в интервыл [10:50]");
+        }
+        else {
+            System.out.println("Число не попадает в интервал [10:50]");
+        }
+
+        System.out.println("ex 3");     // Calendar
+
+        int Feb = 4;
+
+        switch (Feb) {
+            case 1:
+                System.out.println("Tuesday");
+                break;
+            case 2:
+                System.out.println("wednesday");
+                break;
+            case 3:
+                System.out.println("Thursday");
+                break;
+            case 4:
+                System.out.println("Friday");
+                break;
+            case 5:
+                System.out.println("Satuday");
+                break;
+            case 6:
+                System.out.println("Sunday");
+                break;
+            case 7:
+                System.out.println("Monday");
+                break;
+
+
+        }
 
 
     }
