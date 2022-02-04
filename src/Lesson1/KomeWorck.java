200351595ab7238c51ba69514a2794498abb4bd1
Amd <mario95rus@gmail.com>
2022-02-04 21:59:28 +0600

    Homework 2nd day

--- src/Lesson1/KomeWorck.java
+++ src/Lesson1/KomeWorck.java
@@ -2,167 +2,235 @@ package Lesson1;
 
 class HomeWork {
     public static void main(String[] args) {
-        System.out.println("HomeWork exercise 1");
+//        System.out.println("HomeWork exercise 1");
+//
+//        int a = 20;
+//
+//        if (a > 0 && a < 10) {
+//            System.out.println("число положительное однозначное");
+//        }
+//        if (a >= 10 && a < 100) {
+//            System.out.println("Число положительгое двузначное");
+//        }
+//        if (a < 0 && a > -10) {
+//            System.out.println("Число отрицательное однозначное");
+//        }
+//        if (a <= -10 && a > -100) {
+//            System.out.println("Число отрицательное двузначное");
+//        }
+//        System.out.println("Ex2");
+//
+//
+//        int q = 17;
+//        int b = 10;
+//        int c = 7;
+//
+//        if (q + b >= c && q + c >= b && b + c >= q) {
+//            System.out.println("Треугольник существует");
+//        } else {
+//            System.out.println("Треугольник не существует");
+//        }
+//        System.out.println("ex3");
+//
+//        int a1 = 25;
+//
+//        if (a1 > 0) {
+//            a1++;
+//        } else if (a1 < 0) {
+//            a1 = a1 - 2;
+//        } else {
+//            a1 = 10;
+//
+//        }
+//        System.out.println(a1);
+//
+//        System.out.println("ex4");
+//
+//        int x = 1;
+//        int y = 24;
+//        int z = -18;
+//
+//        int countPos = 0;
+//        int countNeg = 0;
+//
+//        if (x > 0) {
+//            countPos++;
+//        } else if (x != 0) {
+//            countNeg++;
+//        }
+//        if (y > 0) {
+//            countPos++;
+//        } else if (y != 0) {
+//            countNeg++;
+//        }
+//        if (z > 0) {
+//            countPos++;
+//        } else if (z != 0) {
+//            countNeg++;
+//        }
+//        System.out.println("Положительных " + countPos + " Отррицательных " + countNeg);
+//
+//        System.out.println("ex5");
+//
+//        int x1 = 19;                    //Написать программу которая определит и выведет на экран большее число
+//        int y1 = 45;
+//
+//        if (x1 > y1) {
+//            System.out.println(x1);
+//        } else if (x1 == y1) {
+//            System.out.println("Чила равны");
+//        } else {
+//            System.out.println(y1);
+//        }
+//        System.out.println("ex6");
+//
+//        int s = 10;
+//        int d = 3;
+//        double res = (double) s / (double) d;
+//
+//        System.out.println(res);
+//
+//        System.out.println("ex7");          //
+//
+//        int programmerCount = 121;
+//        if (programmerCount % 100 / 10 == 1) {
+//            System.out.println(programmerCount + " Програмистов");
+//        }
+//        else {
+//
+//            switch (programmerCount % 10) {
+//                case 1:
+//                    System.out.println(programmerCount + " програмист");
+//                    break;
+//                case 2:
+//                case 3:
+//                case 4:
+//                    System.out.println(programmerCount + " програмиста");
+//                    break;
+//                default:
+//                    System.out.println(programmerCount + " Програмистов");
+//            }
+//        }
+//        System.out.println("HomeWwork 2 ex 1");     //Создать прог. проверяющую и ссобщ на экран чет или не чет
+//
+//        int n = 13;
+//
+//        if (n % 10 / 2 == 0) {
+//            System.out.println("Число четное");
+//        }
+//        else {
+//            System.out.println("Число нечетное");
+//
+//        }
+//
+//        System.out.println("ex 2");     // написать прог опред интервал 10:50 // как 1е и 2е зд объединить и надо ли это
+//
+//        int n1 = 13;
+//
+//        if (n1 > 10 && n1 < 50){
+//            System.out.println("Число попадает в интервыл [10:50]");
+//        }
+//        else {
+//            System.out.println("Число не попадает в интервал [10:50]");
+//        }
+//
+//        System.out.println("ex 3");     // Calendar
+//
+//        int feb = 4;
+//
+//        switch (feb) {
+//            case 1:
+//                System.out.println("Tuesday");
+//                break;
+//            case 2:
+//                System.out.println("wednesday");
+//                break;
+//            case 3:
+//                System.out.println("Thursday");
+//                break;
+//            case 4:
+//                System.out.println("Friday");
+//                break;
+//            case 5:
+//                System.out.println("Satuday");
+//                break;
+//            case 6:
+//                System.out.println("Sunday");
+//                break;
+//            case 7:
+//                System.out.println("Monday");
+//                break;
+//
+//
+//        }
+//        System.out.println("work");
+//
+//        int i1 = 10;
+//
+//        while (i1 >= 0){
+//            System.out.println(i1);
+//            i1--;
+//        }
+//
+//
+//        for (int i = 0;i < 10; i--) {
+//            System.out.println(i);
+//        }
+//
+//        int x1 = 0;
+//        boolean isStop = true;
+//
+//        while (isStop){
+//            System.out.println(x1);
+//            if (x1 == 10){
+//                isStop =false;
+//            }
+//            x1==;
+//        }
+//        for (int i = 0; i < 10; i++) {
+//            for (int j = 0; j < 10; j++) {
+//
+//            }
+//
+//
+//        }
 
-        int a = 20;
-
-        if (a > 0 && a < 10) {
-            System.out.println("число положительное однозначное");
-        }
-        if (a >= 10 && a < 100) {
-            System.out.println("Число положительгое двузначное");
-        }
-        if (a < 0 && a > -10) {
-            System.out.println("Число отрицательное однозначное");
-        }
-        if (a <= -10 && a > -100) {
-            System.out.println("Число отрицательное двузначное");
-        }
-        System.out.println("Ex2");
-
-
-        int q = 17;
-        int b = 10;
-        int c = 7;
-
-        if (q + b >= c && q + c >= b && b + c >= q) {
-            System.out.println("Треугольник существует");
-        } else {
-            System.out.println("Треугольник не существует");
-        }
-        System.out.println("ex3");
-
-        int a1 = 25;
-
-        if (a1 > 0) {
-            a1++;
-        } else if (a1 < 0) {
-            a1 = a1 - 2;
-        } else {
-            a1 = 10;
-
-        }
-        System.out.println(a1);
-
-        System.out.println("ex4");
-
-        int x = 1;
-        int y = 24;
-        int z = -18;
-
-        int countPos = 0;
-        int countNeg = 0;
-
-        if (x > 0) {
-            countPos++;
-        } else if (x != 0) {
-            countNeg++;
-        }
-        if (y > 0) {
-            countPos++;
-        } else if (y != 0) {
-            countNeg++;
-        }
-        if (z > 0) {
-            countPos++;
-        } else if (z != 0) {
-            countNeg++;
-        }
-        System.out.println("Положительных " + countPos + " Отррицательных " + countNeg);
-
-        System.out.println("ex5");
-
-        int x1 = 19;                    //Написать программу которая определит и выведет на экран большее число
-        int y1 = 45;
-
-        if (x1 > y1) {
-            System.out.println(x1);
-        } else if (x1 == y1) {
-            System.out.println("Чила равны");
-        } else {
-            System.out.println(y1);
-        }
-        System.out.println("ex6");
-
-        int s = 10;
-        int d = 3;
-        double res = (double) s / (double) d;
-
-        System.out.println(res);
-
-        System.out.println("ex7");          //
-
-        int programmerCount = 121;
-        if (programmerCount % 100 / 10 == 1) {
-            System.out.println(programmerCount + " Програмистов");
-        }
-        else {
-
-            switch (programmerCount % 10) {
-                case 1:
-                    System.out.println(programmerCount + " програмист");
-                    break;
-                case 2:
-                case 3:
-                case 4:
-                    System.out.println(programmerCount + " програмиста");
-                    break;
-                default:
-                    System.out.println(programmerCount + " Програмистов");
-            }
-        }
-        System.out.println("HomeWwork 2 ex 1");     //Создать прог. проверяющую и ссобщ на экран чет или не чет
-
-        int n = 13;
-
-        if (n % 10 / 2 == 0) {
-            System.out.println("Число четное");
-        }
-        else {
-            System.out.println("Число нечетное");
-
-        }
-
-        System.out.println("ex 2");     // написать прог опред интервал 10:50 // как 1е и 2е зд объединить и надо ли это
-
-        int n1 = 13;
-
-        if (n1 > 10 && n1 < 50){
-            System.out.println("Число попадает в интервыл [10:50]");
-        }
-        else {
-            System.out.println("Число не попадает в интервал [10:50]");
-        }
-
-        System.out.println("ex 3");     // Calendar
-
-        int Feb = 4;
-
-        switch (Feb) {
-            case 1:
-                System.out.println("Tuesday");
-                break;
-            case 2:
-                System.out.println("wednesday");
-                break;
-            case 3:
-                System.out.println("Thursday");
-                break;
-            case 4:
-                System.out.println("Friday");
-                break;
-            case 5:
-                System.out.println("Satuday");
-                break;
-            case 6:
-                System.out.println("Sunday");
-                break;
-            case 7:
-                System.out.println("Monday");
-                break;
+//        for (int i = 1; i <= 10; i++) {
+//
+//            if (i % 2 != 0){
+//              continue;
+//            }
+//            System.out.print(i + " ");
+//
+//        }
+//        int gg = 142678;
+//        int count = 0;
+//        while (gg > 0) {
+//            gg = gg / 10;
+//            count++;
+//        }
+//
+//        System.out.println(count);
+//
+//        System.out.println("cw 3");
+//
+//        for (int i = 1; i < 10; i++) {
+//            for (int j = 1; j < 10; j++) {
+//                System.out.print(j * i + "\t");
+//
+//            }
+//            System.out.println();
+//
+//        }
+//        int boofer = 1;
+//        for (int i = 0; i < 20; i++) {
+//            boofer = boofer * 2;
+//
+//            System.out.print(boofer + "\t");
+//
+//        }
 
 
-        }
 
 
     }
