2e3a5071e930babd443651628ab246af8affcede
Amd <mario95rus@gmail.com>
2022-02-15 20:56:25 +0600

    HomeWork3 3

--- src/Lesson3/HomeWork3.java
+++ src/Lesson3/HomeWork3.java
@@ -2,12 +2,13 @@ package Lesson3;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class HomeWork3 {
     public static void main(String[] args) {
 
 
-//        System.out.println("Задача 1");
+//        System.out.println("Задача 1");               // Задача 1
 //
 //        int[] array;
 //        int res = 0;
@@ -41,7 +42,7 @@ public class HomeWork3 {
 //        }
 //        System.out.println();
 //
-//        System.out.println(" Задача 2");
+//        System.out.println(" Задача 2");          // Задача 2
 //
 //        int size = 0;
 //        int[] arr;
@@ -85,7 +86,7 @@ public class HomeWork3 {
 //        System.out.println();
 //
 //
-//        System.out.println(" Задача 3");
+//        System.out.println(" Задача 3");              // Задача 3
 //
 //
 //        int[] ara = new int[15];
@@ -112,7 +113,7 @@ public class HomeWork3 {
 //
 //        System.out.println();
 //
-//        System.out.println("Задача 4");
+//        System.out.println("Задача 4");           // Задача 4
 //
 //        int[] array4 = new int[20];
 //        Random rnd4 = new Random();
@@ -135,9 +136,45 @@ public class HomeWork3 {
 //
 //        System.out.println();
 //
+//        System.out.println("Задача 5");             // Задача 5
+//
+//        int[] mas51 = new int[5];
+//        int[] mas52 = new int[5];
+//        Random rnd5 = new Random();
+//
+//        for (int i = 0; i < 5; i++) {
+//            mas51[i] = rnd5.nextInt(15);
+//            mas52[i] = rnd5.nextInt(15);
+//        }
+//        System.out.println(Arrays.toString(mas51));
+//        System.out.println(Arrays.toString(mas52));
+//
+//        int avg51;
+//        int avg52;
+//        int calculatin51 = 0;
+//        int calculatin52 = 0;
+//
+//        for (int i = 0; i < mas51.length; i++) {
+//            calculatin51 = calculatin51 + mas51[i];
+//        }
+//        avg51 = calculatin51 / 5;
+//        System.out.println(avg51);
+//
+//        for (int i = 0; i < mas52.length; i++) {
+//            calculatin52 = calculatin52 + mas52[i];
+//        }
+//        avg52 = calculatin52 / 5;
+//        System.out.println(avg52);
+//
+//        if (avg51 > avg52) {
+//            System.out.println("Среднее арифметическое значение 1го массива больше");
+//        } else if (avg51 == avg52) {
+//            System.out.println("Средние фрифметические значений равны");
+//        } else {
+//            System.out.println("Среднее арифметическое 2го массива юольше");
+//        }
 
-
-//        System.out.println("Задача 6");
+//        System.out.println("Задача 6");           // Задача 6
 //
 //        int[] mas6 = new int[12];
 //        Random rnd6 = new Random();
@@ -161,7 +198,7 @@ public class HomeWork3 {
 //        }
 //        System.out.println(pzm6 + " " + indexmas6);
 
-//        System.out.println("Задача 7");
+//        System.out.println("Задача 7");                   // Задача 7
 //
 //        int[] mas7 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,};
 //        Random rnd7 = new Random();
@@ -170,6 +207,7 @@ public class HomeWork3 {
 //            mas7[i] = rnd7.nextInt(9);
 //        }
 //        System.out.println(Arrays.toString(mas7));
+
 //        Boolean isStop = false;
 //        String str7 = "Элементы различны";
 //
@@ -192,6 +230,36 @@ public class HomeWork3 {
 //        }
 //        System.out.println(str7);
 
+        System.out.println("Задача 8");             // Задача 8
 
+        Scanner scan8 = new Scanner(System.in);
+
+        System.out.println("Введите положительное число");
+
+        int num8 = scan8.nextInt();
+        int[]array8;
+
+
+        while (true) {
+
+            if (num8 < 0 || num8 == 0) {
+                System.out.println("Вы ввели неверное число,попробуйте еще раз");
+                scan8.next();
+            }
+
+            else {  array8 = new int[num8];
+                Random rnd8 = new Random();
+                for (int i = 0; i < array8.length; i++) {
+                    array8[i] = rnd8.nextInt(15);
+                }
+                System.out.println(Arrays.toString(array8));
+                break;
+
+
+            }
+
+
+        }
     }
+
 }
